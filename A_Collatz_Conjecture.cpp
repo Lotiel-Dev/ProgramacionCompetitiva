413729b261f606a5a8309b00967ab6874f19b623
Lotiel-Dev <karlos817gerceis@gmail.com>
2025-09-09 12:03:39 -0400

    problema: https://codeforces.com/contest/2137/problem/A

---
+++ A_Collatz_Conjecture.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int k, x;
+        cin >> k >> x;
+ 
+        while (k--) {
+            if ((x - 1) % 3 == 0 && (x - 1) / 3 > 0 && ((x - 1) / 3) % 2 == 1) {
+                x = (x - 1) / 3;
+            } else {
+                x = x * 2;
+            }
+        }
+        cout << x << "\n";
+    }
+    return 0;
+}
