b5d08ebb051a8cd614f57478b6b9d78906f849c1
Lotiel-Dev <karlos817gerceis@gmail.com>
2025-09-13 13:18:05 -0400

    Respuesta

---
+++ A_Another_Divisibility_Problem.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+   
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        ll x;
+        cin >> x;
+        
+        cout << x*2 << endl;
+    }
+    
+    return 0;
+
+    
+
+}
